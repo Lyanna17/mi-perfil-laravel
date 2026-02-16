2cae3bb3c3b73e3327304cb2aa9279007693fc1c
Abigail Vargas Argüello <abivargasargu17@gmail.com>
2026-02-15 20:01:45 -0500

    Creacion de rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/perfil', function () {
+    return view('perfil');
+});
+
+Route::get('/perfil/intereses', function () {
+    return view('intereses');
+});
+
+Route::get('/perfil/habilidades', function () {
+    return view('habilidades');
+});
+
+Route::get('/perfil/metas', function () {
+    return view('metas');
 });
